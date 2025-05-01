37af7ea84b914ce10a173fab4906f3ab19f169b1
An <nguyenvanan7580@gmail.com>
2025-05-01 15:57:54 +0700

    return

--- server.js
+++ server.js
@@ -62,5 +62,7 @@ const server = app.listen(process.env.PORT || 3000, () => {
 const {createAdmin} = require('./createadmin');
 createAdmin(); // Tạo admin nếu chưa tồn tại
 
+socketService(server);
+
 const io = socketService(server); // Sửa: nhận lại io
 app.set('io', io);
